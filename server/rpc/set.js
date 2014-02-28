13da14c1074dcd35c8664d490bb7fc2b7c41ed2a
Maciej Małecki <me@mmalecki.com>
2014-03-01 00:45:28 +0100

    Implement set for price settings

--- server/rpc/set.js
+++ server/rpc/set.js
@@ -1,3 +1,8 @@
+var LamassuConfig = require('lamassu-config');
+
+var psql = process.env.DATABASE_URL || 'postgres://lamassu:lamassu@localhost/lamassu';
+var config = new LamassuConfig(psql);
+
 exports.actions = function(req, res, ss) {
 
   req.use('session')
@@ -5,10 +10,12 @@ exports.actions = function(req, res, ss) {
   return {
 
     price: function(data) {
-
-      console.log('set new price data')
-      console.log(data)
-
+      config.load(function (err, results) {
+        if (err) return res(err);
+        results.config.exchanges.settings.commission = data.commission;
+        results.config.exchanges.plugins.current.ticker = data.provider;
+        config._updateConfig(undefined, results.config, res);
+      });
     }, 
     
     wallet: function(data){
