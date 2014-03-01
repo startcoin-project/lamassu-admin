9b89fb669d12015e825aae23e71d168bb2c52da0
Maciej Małecki <me@mmalecki.com>
2014-03-01 16:35:35 +0100

    Clean the wallet test

--- test/rpcSetTest.js
+++ test/rpcSetTest.js
@@ -1,6 +1,13 @@
 var assert = require('assert');
 var ss = require('socketstream').start();
 
+var blockchain = {
+  provider: 'blockchain',
+  guid: '85d9dc37-85ce-4d84-a0cc-48d8627b74bc',
+  password: 'horse battery stapler',
+  fromAddress: '31uEbMgunupShBVTewXjtqbBv5MndwfXhb'
+};
+
 describe('lamassu-admin/rpc/set', function() {
   it('should set correct price settings', function(done) {
     ss.rpc('set.price', {
@@ -19,18 +26,15 @@ describe('lamassu-admin/rpc/set', function() {
   });
 
   it('should set correct wallet settings', function(done) {
-    ss.rpc('set.wallet', {
-      provider: 'blockchain',
-      guid: 'foo',
-      password: 'bar'
-    }, function(setResult) {
+    ss.rpc('set.wallet', blockchain, function(setResult) {
       assert(!setResult[0], 'setting commission should succeed');
 
       ss.rpc('get.wallet', function(getResult) {
         assert(!getResult[0], 'getting price after setting should succeed');
 
-        assert.equal(getResult[1].guid, 'foo', 'guid should be set correctly');
-        assert.equal(getResult[1].password, 'bar', 'password should be set correctly');
+        assert.equal(getResult[1].guid, blockchain.guid, 'guid should be set correctly');
+        assert.equal(getResult[1].password, blockchain.password, 'password should be set correctly');
+        assert.equal(getResult[1].fromAddress, blockchain.fromAddress, 'fromAddress should be set correctly');
         done();
       });
     });
