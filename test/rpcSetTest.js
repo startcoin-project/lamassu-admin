bf1e4b24e61a1cb630556c458aaf74bc70d6ee81
Maciej Małecki <me@mmalecki.com>
2014-03-01 12:27:23 +0100

    Test setting price

--- test/rpcSetTest.js
+++ test/rpcSetTest.js
@@ -6,10 +6,29 @@ describe('lamassu-admin/rpc/set', function() {
     ss.rpc('set.price', { commission: 1.5 }, function(params) {
       assert(!params[0], 'setting commission should succeed');
 
-      ss.rpc('get.price', function (params) {
+      ss.rpc('get.price', function(params) {
         assert(!params[0], 'getting price after setting should succeed');
 
-        assert.equal(params[0].commission, 1.5, 'commission should be set properly');
+        assert.equal(params[1].commission, 1.5, 'commission should be set properly');
+      });
+
+      done();
+    });
+  });
+
+  it('should set correct wallet settings', function(done) {
+    ss.rpc('set.price', {
+      provider: 'blockchain',
+      guid: 'foo',
+      password: 'bar'
+    }, function(params) {
+      assert(!params[0], 'setting commission should succeed');
+
+      ss.rpc('get.price', function(params) {
+        assert(!params[0], 'getting price after setting should succeed');
+
+        assert.equal(params[1].guid, 'foo', 'guid should be set correctly');
+        assert.equal(params[1].password, 'bar', 'password should be set correctly');
       });
 
       done();
