e0dff8d41e2bb1a2816cc8b876f304fe71c5c298
Maciej Małecki <me@mmalecki.com>
2014-03-01 15:00:53 +0100

    Rename variables to make test clearer

--- test/rpcGetTest.js
+++ test/rpcGetTest.js
@@ -6,6 +6,7 @@ describe('lamassu-admin/rpc/get', function() {
     ss.rpc('get.price', function(params) {
       assert(!params[0]);
 
+      console.dir(params);
       assert.equal(params[1].commission, 1.0);
       assert.equal(params[1].provider, 'bitpay');
       assert.equal(params[1].custom_url, null);
--- test/rpcSetTest.js
+++ test/rpcSetTest.js
@@ -3,13 +3,13 @@ var ss = require('socketstream').start();
 
 describe('lamassu-admin/rpc/set', function() {
   it('should set correct price settings', function(done) {
-    ss.rpc('set.price', { commission: 1.5 }, function(params) {
-      assert(!params[0], 'setting commission should succeed');
+    ss.rpc('set.price', { commission: 1.5 }, function(setResult) {
+      assert(!setResult[0], 'setting commission should succeed');
 
-      ss.rpc('get.price', function(params) {
-        assert(!params[0], 'getting price after setting should succeed');
+      ss.rpc('get.price', function(getResult) {
+        assert(!getResult[0], 'getting price after setting should succeed');
 
-        assert.equal(params[1].commission, 1.5, 'commission should be set properly');
+        assert.equal(getResult[1].commission, 1.5, 'commission should be set properly');
       });
 
       done();
@@ -21,14 +21,14 @@ describe('lamassu-admin/rpc/set', function() {
       provider: 'blockchain',
       guid: 'foo',
       password: 'bar'
-    }, function(params) {
-      assert(!params[0], 'setting commission should succeed');
+    }, function(setResult) {
+      assert(!setResult[0], 'setting commission should succeed');
 
-      ss.rpc('get.price', function(params) {
-        assert(!params[0], 'getting price after setting should succeed');
+      ss.rpc('get.price', function(getResult) {
+        assert(!getResult[0], 'getting price after setting should succeed');
 
-        assert.equal(params[1].guid, 'foo', 'guid should be set correctly');
-        assert.equal(params[1].password, 'bar', 'password should be set correctly');
+        assert.equal(getResult[1].guid, 'foo', 'guid should be set correctly');
+        assert.equal(getResult[1].password, 'bar', 'password should be set correctly');
       });
 
       done();
