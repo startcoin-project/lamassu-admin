8410929bab3e5e395f5463ca3277cd324fb8fbb5
Maciej Małecki <me@mmalecki.com>
2014-03-27 20:22:59 +0700

    Authenticate against the database

--- server/rpc/user.js
+++ server/rpc/user.js
@@ -1,3 +1,5 @@
+var config = require('../config.js')
+
 exports.actions = function(req, res, ss) {
 
   req.use('session')
@@ -5,19 +7,24 @@ exports.actions = function(req, res, ss) {
   return {
 
     authenticate: function(username, password){      
+      config.authenticateUser(username, password, function(err, authenticated){
+        if (err){
+          return res(err)
+        }
 
-      if (username === 'user' && password === 'pass'){
+        if (authenticated){
 
-        req.session.userId = 1 //session is authenticated when it has a userId
-        req.session.save()
+          req.session.userId = 1 //session is authenticated when it has a userId
+          req.session.save()
 
-        res(null, true)
+          res(null, true)
 
-      }else{
+        }else{
 
-        res(null, false)
+          res(null, false)
 
-      }
+        }
+      })
     }
   }
 }
